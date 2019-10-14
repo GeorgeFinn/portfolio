bdcae1883674ef2ef0218f6388d2f5917af8d9c6
George Finn <george.finn@me.com>
2019-10-14 17:22:22 -0400

    Revert "portfolio migration"

--- src/components/seniordesign/index.js
+++
@@ -1,3 +0,0 @@
-export { default as InfoSlide } from "./InfoSlide";
-export { default as StackSlide } from "./StackSlide";
-export { default as VisitSlide } from "./VisitSlide";
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Portfolio } from "./Portfolio";
-export { default as Resume } from "./Resume";
-export { default as SeniorDesign } from "./SeniorDesign";
